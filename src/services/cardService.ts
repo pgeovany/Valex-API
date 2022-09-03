db45a84af2e3f23c47d2fc4866db0b5e9ecad975
pgeovany <pgeovany@ic.ufrj.br>
2022-09-03 18:48:48 -0300

    refactor: create cardUtils file

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -4,14 +4,18 @@ import cryptr from '../utils/cryptr';
 import encrypt from '../utils/encrypt';
 import * as cardRepository from '../repositories/cardRepository';
 import * as employeeRepository from '../repositories/employeeRepository';
-import * as companyRepository from '../repositories/companyRepository';
+import * as cardUtils from '../utils/cardUtils';
 
 async function generateNewCard(
   employeeId: number,
   cardType: cardRepository.TransactionTypes,
   apiKey: string
 ) {
-  const { employee } = await validateNewCardInfo(employeeId, cardType, apiKey);
+  const { employee } = await cardUtils.validateNewCardInfo(
+    employeeId,
+    cardType,
+    apiKey
+  );
 
   const card = generateCardInfo(employee, cardType);
 
@@ -19,12 +23,12 @@ async function generateNewCard(
 }
 
 async function activateCard(id: number, cvv: string, password: string) {
-  const card = await getCardById(id);
+  const card = await cardUtils.getCardById(id);
 
-  validateCardCvv(card.securityCode, cvv);
-  isExpired(card.expirationDate);
+  cardUtils.validateCardCvv(card.securityCode, cvv);
+  cardUtils.isExpired(card.expirationDate);
 
-  if (isActivated(card.password)) {
+  if (cardUtils.isActivated(card.password)) {
     throw {
       type: 'error_bad_request',
       message: 'This card has already been activated!',
@@ -73,86 +77,6 @@ function getFormattedName(fullName: string): string {
   });
 
   return [firstName, ...middleNamesInitials, lastName].join(' ');
-}
-
-async function validateNewCardInfo(
-  employeeId: number,
-  cardType: cardRepository.TransactionTypes,
-  apiKey: string
-) {
-  const company = await companyRepository.findByApiKey(apiKey);
-  if (!company) {
-    throw {
-      type: 'error_unauthorized',
-      message: 'Invalid API key!',
-    };
-  }
-
-  const employee = await employeeRepository.findById(employeeId);
-  if (!employee) {
-    throw {
-      type: 'error_not_found',
-      message: 'Employee not found!',
-    };
-  }
-
-  const card = await cardRepository.findByTypeAndEmployeeId(
-    cardType,
-    employeeId
-  );
-
-  if (card) {
-    throw {
-      type: 'error_conflict',
-      message: 'The given employee already have a card of this type.',
-    };
-  }
-
-  return { employee };
-}
-
-async function getCardById(id: number) {
-  const card = await cardRepository.findById(id);
-
-  if (!card) {
-    throw {
-      type: 'error_not_found',
-      message: 'Card not found! Please provide a valid card id.',
-    };
-  }
-
-  return card;
-}
-
-function isExpired(expirationDate: string) {
-  const currentDate = dayjs().format('MM/YY');
-
-  if (dayjs(expirationDate).diff(currentDate) >= 0) {
-    return false;
-  }
-
-  throw {
-    type: 'error_bad_request',
-    message: 'This card is expired!',
-  };
-}
-
-function isActivated(password: string) {
-  if (password !== null) {
-    return true;
-  }
-  return false;
-}
-
-function validateCardCvv(encryptedCvv: string, receivedCvv: string) {
-  if (cryptr.decrypt(encryptedCvv) === receivedCvv) {
-    return;
-  }
-
-  throw {
-    type: 'error_unauthorized',
-    message: 'Please provide a valid security code!',
-  };
 }
 
 export { generateNewCard, activateCard };
---
+++ src/utils/cardUtils.ts
@@ -0,0 +1,93 @@
+import * as cardRepository from '../repositories/cardRepository';
+import * as companyRepository from '../repositories/companyRepository';
+import * as employeeRepository from '../repositories/employeeRepository';
+import cryptr from '../utils/cryptr';
+import dayjs from 'dayjs';
+
+async function validateNewCardInfo(
+  employeeId: number,
+  cardType: cardRepository.TransactionTypes,
+  apiKey: string
+) {
+  const company = await companyRepository.findByApiKey(apiKey);
+  if (!company) {
+    throw {
+      type: 'error_unauthorized',
+      message: 'Invalid API key!',
+    };
+  }
+
+  const employee = await employeeRepository.findById(employeeId);
+  if (!employee) {
+    throw {
+      type: 'error_not_found',
+      message: 'Employee not found!',
+    };
+  }
+
+  const card = await cardRepository.findByTypeAndEmployeeId(
+    cardType,
+    employeeId
+  );
+
+  if (card) {
+    throw {
+      type: 'error_conflict',
+      message: 'The given employee already have a card of this type.',
+    };
+  }
+
+  return { employee };
+}
+
+async function getCardById(id: number) {
+  const card = await cardRepository.findById(id);
+
+  if (!card) {
+    throw {
+      type: 'error_not_found',
+      message: 'Card not found! Please provide a valid card id.',
+    };
+  }
+
+  return card;
+}
+
+function isExpired(expirationDate: string) {
+  const currentDate = dayjs().format('MM/YY');
+
+  if (dayjs(expirationDate).diff(currentDate) >= 0) {
+    return false;
+  }
+
+  throw {
+    type: 'error_bad_request',
+    message: 'This card is expired!',
+  };
+}
+
+function isActivated(password: string) {
+  if (password !== null) {
+    return true;
+  }
+  return false;
+}
+
+function validateCardCvv(encryptedCvv: string, receivedCvv: string) {
+  if (cryptr.decrypt(encryptedCvv) === receivedCvv) {
+    return;
+  }
+
+  throw {
+    type: 'error_unauthorized',
+    message: 'Please provide a valid security code!',
+  };
+}
+
+export {
+  validateNewCardInfo,
+  getCardById,
+  isExpired,
+  isActivated,
+  validateCardCvv,
+};
