0d3f499ad5354aea99d57ed13af6f6b8621bb48a
pgeovany <pgeovany@ic.ufrj.br>
2022-09-04 14:50:19 -0300

    chore: setup for deploy on heroku

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,20 @@
-import dotenv from 'dotenv';
 import pg from 'pg';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
 const { Pool } = pg;
-export const connection = new Pool({
+
+const configDatabase: pg.ClientConfig = {
   connectionString: process.env.DATABASE_URL,
-});
+};
+
+if (process.env.MODE === 'PROD') {
+  configDatabase.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const connection = new Pool(configDatabase);
+
+export { connection };
